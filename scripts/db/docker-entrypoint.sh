b56c049a5df407a627acfb6c9db321bdb338ca28
Adetola Bewaji <adetola013@gmail.com>
2022-04-09 11:48:00 +0100

    config updated

--- scripts/db/docker-entrypoint.sh
+++ scripts/db/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-npx sequelize-cli db:migrate
+NODE_ENV=development npx sequelize-cli db:drop --env development
 
-npm run dev
+NODE_ENV=development npx sequelize-cli db:create --env development
+
+NODE_ENV=development npx sequelize-cli db:migrate --env development
