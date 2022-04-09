feaf1c6b70c851b21d5a09c003f1599086c7a43b
Adetola Bewaji <adetola013@gmail.com>
2022-04-09 12:26:51 +0100

    test updated

--- tests/authorization.test.js
+++ tests/authorization.test.js
@@ -1,27 +1,23 @@
-const fetch = require('node-fetch');
+import request from 'supertest';
+import app from '../src/server';
 
-const apiBase = 'http://0.0.0.0:3000/auth';
+const fetch = require('node-fetch');
 
 const params = { 
     "username": "basic-thomas",
     "password": "sR-_pcoow-27-6PAwCD8"
 }
 
-describe('Get auth', () => {
-    it('should get /auth', async () => {
+describe('Get Root', () => {
+    it('should get /dsas', async () => {
 
-        
-      
-        const response = await fetch(apiBase, {
-            method: 'POST',
-            body: JSON.stringify(params),
-            headers: { 'Content-Type': 'application/json' }
-        })
-
-        expect(response.status).toBe(200);
+    const response = await request(app)
+        .post('/auth')
+        .send(params)
+        expect(response.statusCode).toBe(200);
 
     
-    })
+})
 });
 
 
