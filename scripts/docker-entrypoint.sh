8c1bac8984cb1485a47806263ae37ba745afc70a
Adetola Bewaji <adetola013@gmail.com>
2022-04-11 16:13:26 +0100

    deleted

--- scripts/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-NODE_ENV=development npx sequelize-cli db:drop --env development
-
-NODE_ENV=development npx sequelize-cli db:create --env development
-
-NODE_ENV=development npx sequelize-cli db:migrate --env development
