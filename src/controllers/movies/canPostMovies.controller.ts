e821b2ca08edc7e2a7ee3f3e1f53c15f55c5acc8
Adetola Bewaji <adetola013@gmail.com>
2022-04-17 18:29:00 +0100

    canPostMovies moved to controllers

---
+++ src/controllers/movies/canPostMovies.controller.ts
@@ -0,0 +1,34 @@
+import countMovies from '../../db/movies/service.countMovies';
+import logger from '../../utility/logger';
+
+const canPostMovies = async (req:any, res:any, next:any)=>{
+
+    try {
+
+      const { userId, role } = req;
+
+      const moviesLength = await countMovies(userId);
+
+    if (role === 'basic' && moviesLength>4 ){
+
+        const error = 'You have exceeded 5 movies. Become a premium user for unlimited plan';
+        logger.error(`402 - ${error} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+        res.status(402).json({ error });
+
+      } else {
+
+        next();
+
+      }
+
+    } catch (error:any) {
+    
+      logger.error(`${error.status || 500} - ${res.statusMessage} - ${error.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);    
+      res.status(500).json({ error: "An error has occurred. Please try again" });
+
+    }
+  
+  }
+  
+
+  export default canPostMovies;
