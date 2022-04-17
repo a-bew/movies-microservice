cf9d763203a7b0d9a8f4a87bf69e31fa9c5e0adf
Adetola Bewaji <adetola013@gmail.com>
2022-04-17 18:30:18 +0100

    winston logger added

---
+++ src/utility/logger.ts
@@ -0,0 +1,25 @@
+import winston from 'winston';
+import fs from 'fs';
+
+const logDir = 'logs';
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      //
+      // - Write all logs with importance level of `error` or less to `error.log`
+      // - Write all logs with importance level of `info` or less to `combined.log`
+      //
+      new winston.transports.File({ filename: '../../logs/error.log', level: 'error' }),
+      new winston.transports.File({ filename: '../../logs/combined.log' }),
+    ],
+  });
+  
+
+export default logger;
